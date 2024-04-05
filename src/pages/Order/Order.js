2f5ece74362097ce96e9b65d5fbf00b3c8acfe32
Navneel Nandran <navneel.nandran@outlook.com>
2024-04-05 10:21:28 -0400

    create colums

--- src/pages/Order/Order.js
+++ src/pages/Order/Order.js
@@ -1,17 +1,20 @@
 import Delivery from "../../components/Delivery/Delivery";
 import OrderHeader from "../../components/OrderHeader/OrderHeader";
+import Paid from "../../components/Paid/Paid";
 import "./Order.scss"
 
 export default function Order() {
     return (
         <main>
             <OrderHeader />
-            <section className="container">
-                <Delivery />
-            </section>
-            <section className="container">
-
-            </section>
+            <article>
+                <section className="container">
+                    <Delivery />
+                </section>
+                <section className="container">
+                    <Paid />
+                </section>
+            </article>
         </main>
     )
 }
