abcaebbed5872fb9dcd0c2b08334c921d397f185
Navneel Nandran <navneel.nandran@outlook.com>
2024-04-05 01:34:47 -0400

    add styling

--- src/components/OrderHeader/OrderHeader.js
+++ src/components/OrderHeader/OrderHeader.js
@@ -4,15 +4,17 @@ import chevron from "../../assets/chevronDown.svg"
 export default function OrderHeader() {
     return (
         <section className="order-header">
-            <img src={arrowLeft} alt="back arrow" className="order-header__image" />
             <section className="order-header__container">
-                <h2 className="order-header__info">Order #1093</h2>
-                <p className="order-header__po">No PO number</p>
+                <img src={arrowLeft} alt="back arrow" className="order-header__image" />
+                <section className="order-header__info">
+                    <h1 className="order-header__order">Order #1093</h1>
+                    <p className="order-header__po">No PO number</p>
+                </section>
             </section>
             <section className="order-header__container">
                 <button className="order-header__btn">Buy Again</button>
                 <button className="order-header__btn">
-                    Buy Again
+                    Manage
                     <img src={chevron} alt="arrow down" className="order-header__icon" />
                 </button>
             </section>
