a18f0fee8b8536bef2bff29772f00702927eec31
Navneel Nandran <navneel.nandran@outlook.com>
2024-04-04 22:50:07 -0400

    basic boilerplate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Order from "./pages/Order/Order";
+import ChatBot from "./pages/ChatBot/ChatBot";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Order />}></Route>
+          <Route path="/chatbot" element={<ChatBot />}></Route>
+        </Routes>
+
+      </BrowserRouter>
     </div>
   );
 }
